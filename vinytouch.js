dbc39c28222449b0b749aa36bf53701983f14da0
viny <vin0001@naver.com>
2014-05-19 18:29:12 +0900

    Update vinytouch.js

--- vinytouch.js
+++ vinytouch.js
@@ -184,7 +184,7 @@ var _isAndroid = (/android/gi).test(navigator.appVersion),
 			if(options != undefined){
 				pollingTime = options["time"];
 			}else{
-				pollingTime = 100
+				pollingTime = 100;
 			}
 
 			this.component = this;
